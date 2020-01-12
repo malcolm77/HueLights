e321963cdb75b851c469e03b94e6865e733f0599
malcolm chalmers <malcolmchalmers@hotmail.com>
2020-01-12 19:19:38 +1100

    playing with how to get info from various nodes

--- get-devices.py
+++ get-devices.py
@@ -6,14 +6,30 @@
 import json 
 import requests
 
+print ("-----------------------------------------------------------------------------")
+
 bridgeIP = "192.168.1.126"
 userName = "qxb5PJleolvND8RAvqokpuj1eM7o4N--9bdavDAs"
 
 response = requests.get("http://" + bridgeIP + "/api/" + userName + "/")
 devices = json.loads(response.text)
 
-print ( devices["groups"] )
+# you can create a "sub-collection" of just certain items like this
+# groups = devices["groups"]
+# configs = devices["config"]
+# but you don't need to
 
+# get the name and ip address of the hub
+print ( "Your hub is called " + devices["config"]["name"] + " and is on IP Address " + devices["config"]["ipaddress"] )
+
+# get the name of group number 5
+# print ( devices["groups"]["5"]["name"] )
+
+print ("You have the following groups configure on your hub:")
+for group in devices["groups"]:
+    print ( "    " + devices["groups"][group]["name"] )
+
+# example of how to get sub nob elements from another program
 # for device in devices:
     #print ( devices[device] )
     # print datastore["office"]["parking"]["style"]
